a0f8e020bf7e587a33488170a755bb76456943f2
SweetTooth-1 <saimap001122@gmail.com>
2025-01-07 14:41:58 +0000

    add new files

---
+++ .devcontainer/01_basics/01variables.js
@@ -0,0 +1,14 @@
+const accountid = "Saima01"
+let accountEmail = "saimap0011@gmail.com"
+var accountPassword = "123123"
+accountCity = "west bengal"
+accountState;
+
+accountEmail = "sam@gmail.com"
+accountPassword = "232323"
+accountCity = "hyderabad"
+
+
+console.log(accountid);
+
+console.table([accountEmail, accountPassword, accountCity, accountState]);
